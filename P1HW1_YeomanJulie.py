8cf3d5ef701b214112885c809dd32ffb8f90dc61
YeomanJ7782 <yeomanj7782@student.faytechcc.edu>
2024-11-20 23:11:43 -0500

    collect/process user information and display

---
+++ P1HW1_YeomanJulie.py
@@ -0,0 +1,26 @@
+# Julie Yeoman
+# 9/28/2024
+# P1HW1
+# This program collects information from user, processes information collected and displays result to user
+
+print("--------Calculating Exponents--------")
+print()
+print()
+base= int(input("Enter an integer as the base value:"))
+exponent= int(input("Enter an integer as the exponent:"))
+result= base ** exponent
+print()
+print()
+print(f"{base} raised to the power of {exponent} is {result}!!")
+print()
+print()
+print("--------Addition and Subtraction--------")
+print()
+print()
+num1= int(input("Enter a starting integer:"))
+num2= int(input("Enter an integer to add:"))
+num3= int(input("Enter an integer to subtract:"))
+final_result= num1 + num2 - num3
+print()
+print()
+print(f"{num1} + {num2} - {num3} is equal to {final_result}")
